2e59918f4be18fc89220f54a8ca213dab3297285
21Ravan12 <asgarovravan@gmail.com>
2025-02-05 12:27:25 +0400

    Update socket.js

--- js/socket.js
+++ js/socket.js
@@ -1,75 +1,78 @@
-const socket = io("http://192.168.0.158:3002");
-let activeRooms = [];
-let activeRoomId = null;
+        const socket = io("http://192.168.0.158:3002");
+        let activeRooms = [];
+        let activeRoomId = null;
+    
+        function debugInfo(message, data) {
+            console.log(`[DEBUG] ${message}:`, data);
+        }
+    
+        function joinGame(roomId, color) {
+            if (!roomId || !color) {
+                console.error("[ERROR] Oda ID veya renk eksik!");
+                return;
+            }
 
-function debugInfo(message, data) {
-    console.log(`[DEBUG] ${message}:`, data);
-}
+            activeRoomId = roomId;
+            debugInfo("Sending joinRoom event for room", { roomId, color });
+            socket.emit("joinRoom", { roomId, color });
+            return true;
+        }
+    
+        function getRoomList() {
+            debugInfo("Requesting room list", {});
+            socket.emit("getRooms");
+        }
+    
+        function getColor() {
+            socket.emit("getColor",selectedRoomId);
+            
+        }
 
-function joinGame(roomId, color) {
-    if (!roomId || !color) {
-        console.error("[ERROR] Oda ID veya renk eksik!");
-        return;
-    }
+        socket.on("yourColor", (color) => {
+            debugInfo("Assigned color", color);
+            yourColor=color;
+        });
 
-    activeRoomId = roomId;
-    debugInfo("Sending joinRoom event for room", { roomId, color });
-    socket.emit("joinRoom", { roomId, color });
-}
+        socket.on("assignColor", (color) => {
+            debugInfo("Assigned color", color);
 
-function getRoomList() {
-    debugInfo("Requesting room list", {});
-    socket.emit("getRooms");
-}
-
-function getColor() {
-    socket.emit("getColor",selectedRoomId);
-}
-
-socket.on("yourColor", (color) => {
-    yourColor=color;
-});
-
-socket.on("assignColor", (color) => {
-    debugInfo("Assigned color", color);
-
-});
-
-socket.on("error", (message) => {
-    console.error(`[ERROR] ${message}`);
-});
-
-// Rakibin hamlesini dinle
-socket.on("move", (move) => {
-    if (move && move.from && move.to) {
-        debugInfo("Opponent move received", move);
-        handleOpponentMove(move.from.row, move.from.col, move.to.row, move.to.col);
-    } else {
-        console.error("[ERROR] Invalid move data received", move);
-    }
-});
-
-socket.on("roomsList", (rooms) => {
-    debugInfo("Active rooms list received", rooms);
-    activeRooms = rooms;
-    updateGameListUI(); 
-});
-
-socket.on("gameStart", () => {
-    debugInfo("Game started", {});
-});
-
-socket.on("playerCount", (count) => {
-    debugInfo("Player count updated", count);
-});
-
-// Taş hareketini gönder
-function movePieceSocket(fromRow, fromCol, toRow, toCol) {
-    if (!activeRoomId) {
-        console.error("[ERROR] Oyun odası seçilmemiş!");
-        return;
-    }
-    const moveData = { roomId: activeRoomId, from: { row: fromRow, col: fromCol }, to: { row: toRow, col: toCol } };
-    debugInfo("Sending move data", moveData);
-    socket.emit("move", moveData);
-}
+        });
+    
+        socket.on("error", (message) => {
+            console.error(`[ERROR] ${message}`);
+        });
+    
+        // Rakibin hamlesini dinle
+        socket.on("move", (move) => {
+            if (move && move.from && move.to) {
+                debugInfo("Opponent move received", move);
+                handleOpponentMove(move.from.row, move.from.col, move.to.row, move.to.col);
+            } else {
+                console.error("[ERROR] Invalid move data received", move);
+            }
+        });
+    
+        socket.on("roomsList", (roomInfo) => {
+            activeRooms = roomInfo;
+            debugInfo(activeRooms);
+            updateGameListUI(); 
+        });
+    
+        socket.on("gameStart", () => {
+            debugInfo("Game started", {});
+        });
+    
+        socket.on("playerCount", (count) => {
+            debugInfo("Player count updated", count);
+        });
+    
+        // Taş hareketini gönder
+        function movePieceSocket(fromRow, fromCol, toRow, toCol) {
+            if (!activeRoomId) {
+                console.error("[ERROR] Oyun odası seçilmemiş!");
+                return;
+            }
+            const moveData = { roomId: activeRoomId, from: { row: fromRow, col: fromCol }, to: { row: toRow, col: toCol } };
+            debugInfo("Sending move data", moveData);
+            socket.emit("move", moveData);
+        }
